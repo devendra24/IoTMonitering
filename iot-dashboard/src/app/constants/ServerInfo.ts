72249a4ca1c555f55b66fcc2592baec5e829e556
Devendra <devendra_dewangan@outlook.com>
2025-10-20 00:05:05 +0530

    Removed old fornt End

--- iot-dashboard/src/app/constants/ServerInfo.ts
+++
@@ -1,6 +0,0 @@
-export const PROTOCOL:string = "http";
-export const HOSTNAME:string = "localhost";
-export const PORT:number =5050;
-export const BASE_URL = `${PROTOCOL}://${HOSTNAME}:${PORT}`;
-export const AUTH_ROUTE: string = "/api/auth";
-export const DEVICE_ROUTE:string = "/api/devices";
