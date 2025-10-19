bcb81a7f86bde45c6da60484890b260e8624976e
Devendra <devendra_dewangan@outlook.com>
2025-10-19 21:21:05 +0530

    Removed Auth

--- src/Models/User.cs
+++ src/Models/User.cs
@@ -1,19 +1,8 @@
 ﻿namespace IoTMonitoring.Models
 {
-    public enum Role
-    {
-        User,
-        Admin
-    }
-
     public class User
     {
         public int Id { get; set; }
-        public string Username { get; set; } = string.Empty;
-        public string PasswordHash { get; set; } = string.Empty;
-
-        public Role Role  { get; set; } = Role.User;
-
         public ICollection<Device> Devices { get; set; } = new List<Device>();
     }
 }
--- src/Program.cs
+++ src/Program.cs
@@ -1,7 +1,4 @@
-using System.Text;
 using IoTMonitoring.Data;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
 using Microsoft.EntityFrameworkCore;
 using IoTMonitoring.Hubs;
 using IoTMonitoring.Grpc;
@@ -25,25 +22,6 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-var key = Encoding.ASCII.GetBytes(builder.Configuration["Jwt:Key"]);
-
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-.AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateIssuerSigningKey = true,
-        ValidIssuer = builder.Configuration["Jwt:Issuer"],
-        ValidAudience = builder.Configuration["Jwt:Audience"],
-        IssuerSigningKey = new SymmetricSecurityKey(key)
-    };
-});
 
 builder.Services.AddControllers();
 builder.Services.AddSignalR();
@@ -61,8 +39,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-app.UseAuthentication();
-app.UseAuthorization();
 app.MapControllers();
 app.MapHub<TelemetryHub>("/hubs/telemetry");
 app.MapGrpcService<TelemetryGrpcService>();
